7aff250d1c1b29125556557c65a01d4c080c9ee3
Mostafa Hafez <mostafa.hafez@nagarro.com>
2026-02-26 09:54:50 +0100

    Appvue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,43 +1,8 @@
-
 <script setup>
-import { ref } from "vue"
-import aussagepaare from "./components/popups/aussagepaare.vue"
-import Passwort from "./components/popups/passwort.vue"
-
-const open = ref(false)
-
-const statements = ref([
-  { correct: true },
-  { correct: false }
-])
-
-const onSave = (data) => {
-  console.log("Gespeichert:", data)
-}
+import PopupVue from './components/PopupVue.vue'
 </script>
-
 <template>
-  <button
-    class="rounded-lg bg-blue-500 px-6 py-2 text-white"
-    @click="open = true"
-  >
-    Popup öffnen
-  </button>
 
-  <!-- <aussagepaare
-    v-model="open"
-    :statements="statements"
-    @save="onSave"
-  /> -->
-
-  <Passwort
-    :open="open"
-    text="Möchten Sie wirklich das Passwort zurücksetzen?"
-    username="MustafaHafez"
-    bt1="Abbrechen"
-    bt2="Löschen"
-    type="red"
-    @cancel="open = false"
-    @confirm="onSave('Passwort zurückgesetzt')"
-  />
+  <PopupVue/>
 </template>
+<style scoped></style>
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,5 +5,12 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(),tailwindcss()],
+  plugins: [vue(),tailwindcss()]
+  ,
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
+  
 })
