6c62c098a8b2fac0250912b9600b23dab52e5140
Philip Neuhäuser <philip.neuhaeuser@rewe-group.com>
2026-02-26 12:07:10 +0100

    added loginpage

---
+++ frontend/src/components/login/NewPasswordPopup.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="w-130 h-130 border-2 rounded-2xl border-border-grey bg-white">
+        <h2 class="text-center mt-10 text-4xl font-bold text-grey-text">Neues Passwort</h2>
+        <div class="flex flex-col items-center mt-15 gap-10">
+            <input type="password" placeholder="Neues Passwort"
+                class="w-full max-w-md px-6 py-3 rounded-full border-2 
+                    border-border-grey hover:bg-gray-100 text-grey-text placeholder-grey-text outline-none focus:border-gray-800 transition" />
+            <input type="password" placeholder="Passwort bestätigen"
+                class="w-full max-w-md px-6 py-3 rounded-full border-2 
+                    border-border-grey hover:bg-gray-100 text-grey-text placeholder-grey-text outline-none focus:border-gray-800 transition" />
+
+            <button class="w-full mt-10 max-w-md py-3 rounded-full bg-main-blue text-white text-lg font-medium
+                shadow-md hover:bg-hover-blue active:bg-blue-400 transition">Bestätigen</button>
+        </div>
+    </div>
+</template>
