c1d4b6269ccd930f4e9b231484a76523eab8b203
Philip Neuhäuser <philip.neuhaeuser@rewe-group.com>
2026-02-26 10:14:06 +0100

    added homepage

---
+++ frontend/src/components/Startseite/Square.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+
+
+const props = defineProps({
+    text: String,
+    routerLink: String
+})
+
+</script>
+
+<template>
+    <router-link :to="'/' + props.routerLink" :key="props.text" class="w-60 h-60 border-main-blue border-2 rounded-2xl flex items-center justify-center text-grey-text 
+            hover:bg-main-blue hover:text-white cursor-pointer text-2xl font-bold">
+        {{ props.text }}
+    </router-link>
+</template>
--- frontend/src/pages/Startseite.vue
+++ frontend/src/pages/Startseite.vue
@@ -1,3 +1,14 @@
+<script setup lang="ts">
+import Square from '../components/Startseite/Square.vue';
+
+</script>
+
 <template>
-    Startseite
+    <h2 class="text-4xl ml-10 mt-15">Hallo, [User-Email]</h2>
+    <div class="ml-10 mt-15 flex gap-15 flex-wrap">
+        <Square text="Aussagenpaare" routerLink="aussagenpaare" />
+        <Square text="Übungen" routerLink="uebungen" />
+        <Square text="Kategorien" routerLink="kategorien" />
+        <Square text="Admindashboard" routerLink="admindashboard" />
+    </div>
 </template>
