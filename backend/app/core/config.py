21421c22ab9013cf9b5c16f653aeec26806c393f
Philip Neuhäuser <philip.neuhaeuser@rewe-group.com>
2026-03-05 10:48:17 +0100

    fix connection

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -5,5 +5,7 @@ load_dotenv()
 
 username = os.getenv("MONGODB_USERNAME")
 password = os.getenv("MONGODB_PASSWORD")
-mongodb_url = f"mongodb+srv://${username}:${password}@gsoosp1.fmv77zg.mongodb.net/"
+mongodb_url = f"mongodb+srv://{username}:{password}@gsoosp1.fmv77zg.mongodb.net/"
 mongodb_db = "mydatabase"
+
+print(mongodb_url)
